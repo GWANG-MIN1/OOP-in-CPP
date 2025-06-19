68a379144d6c7424d1118b6c3656a394bedb5a57
박광민 <rhkdrhkd2222@naver.com>
2025-06-19 14:18:31 +0900

    feat : Add_1_address_operator.cpp example

---
+++ Chapter01_Pointers_and_References/1_address_operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int number = 88;
+
+    // 변수의 값 출력
+    std::cout << "Value of number: " << number << std::endl;
+
+    // 변수의 메모리 주소 출력
+    std::cout << "Address of number: " << &number << std::endl;
+
+    return 0;
+}
