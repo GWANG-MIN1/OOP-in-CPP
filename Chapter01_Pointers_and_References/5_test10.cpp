4d89f26762e844584ae7bec7369d2a45bde73ae2
박광민 <rhkdrhkd2222@naver.com>
2025-06-24 23:14:39 +0900

    Add: 5_test10.cpp

---
+++ Chapter01_Pointers_and_References/5_test10.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+ int & makeInt(int val){  
+ int*p;
+ p = (int *)malloc(sizeof(int));
+ *p =val;
+ return *p;
+ }
+ int main(){
+  int &a = makeInt(3);
+  printf("%d\n",a);
+ }
