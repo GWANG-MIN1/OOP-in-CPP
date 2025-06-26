2f12820ce18803fe4ede4735c1f05ea357fe87d8
박광민 <rhkdrhkd2222@naver.com>
2025-06-26 15:03:27 +0900

    Add: 8_test.c

---
+++ Chapter02_Dynamic_Allocation_and_Linked_Lists/8_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+struct item {
+int score;
+struct item *next;
+};
+int main(){
+    struct item A,B,C,D;
+    struct item *pFirst;
+    struct item *p;
+    A.score = 1; B.score = 2; C.score = 3; D.score = 4;
+    pFirst = &A; A.next = &B; B.next = &C; C.next = &D; D.next = 0;
+    p = pFirst;
+    printf("%d--> ", p -> score);
+    p = p ->next;
+    printf("%d--> ", p -> score);
+    p = p ->next;
+    printf("%d--> ", p -> score);
+    p = p ->next;
+    printf("%d--> ", p -> score);
+    
+    printf("END\n");
+    return 0;
+}
