fac74bedef4649cf212ca6fcd632f245f2ad59ef
박광민 <rhkdrhkd2222@naver.com>
2025-06-29 14:46:42 +0900

    Add : 7_test12.c

---
+++ Chapter01_Pointers_and_References/7_test12.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>  
+int main(){
+ char * str ="abcdefg";  
+ printf("%c\n", *str);  
+ printf("%s\n",str);
+ 
+ str++;  
+ printf("%c\n", *str);  
+ printf("%s\n",str);
+ 
+ str += 2;  
+ printf("%c\n", *str);  
+ printf("%s\n", str);  
+ return 0;
+ }
