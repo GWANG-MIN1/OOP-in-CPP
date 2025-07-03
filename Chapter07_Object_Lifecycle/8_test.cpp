e3edacad5b4bbfd45e967fe7dfe02a1da97edd16
박광민 <rhkdrhkd2222@naver.com>
2025-07-03 09:39:50 +0900

    Add: 8_test.cpp

---
+++ Chapter07_Object_Lifecycle/8_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Rect {
+    int m_width, m_height;
+public:
+    Rect(){m_width= 1, m_height=1;}
+    Rect(int w) {m_width = w , m_height = w;}
+    Rect(int w, int h) {m_width =w, m_height = h;}
+    void area() {cout << "면적 " << m_width * m_height << endl;}
+}; //클래스 선언한 후에 세미콜론 찍기 
+int main(){
+    Rect * r1 = new Rect();
+    r1 -> area();
+    Rect *r2 = new Rect(2);
+    r2 -> area();
+    Rect *r3 = new Rect(3,4);
+    r3 -> area();
+}   
